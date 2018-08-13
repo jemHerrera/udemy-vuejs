3f5d039f3f1a759a7032e0fdc21009f86334422a
Jem Herrera <jhneramuz@gmail.com>
2018-08-12 22:07:43 -0600

    exercise 4

--- Exercise 4/app.js
+++ Exercise 4/app.js
@@ -1,11 +1,34 @@
-new Vue({
+const vue = new Vue({
   el: '#exercise',
   data: {
-
+    effect: true,
+    class1: '',
+    activate: '',
+    myStyles: ''
   },
   methods: {
     startEffect: function() {
-    
+      let vm = this;
+      setInterval(function(){
+        vm.effect = !vm.effect;
+      }, 1000);
+    }
+  },
+  computed:{
+    applyClass: function(){
+      console.log(this.class1);
+      return `${this.class1}`;
+    },
+    activateClass: function(){
+      console.log(this.activate);
+      if (this.activate == 'true') return 'white-font';
+    },
+    applyStyle: function(){
+      return {
+        backgroundColor: this.myStyles,
+        width: '200px',
+        height: '200px'
+      }
     }
   }
 });
